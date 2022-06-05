7d180f357edfa719abc6f0b3287f663a82d7d319
An Phan <quocan.omg@gmail.com>
2022-06-05 19:47:39 +0700

    chat bot api extend

--- server/controllers/chatbotController.js
+++ server/controllers/chatbotController.js
@@ -4,6 +4,7 @@ const uuid = require("uuid");
 const dialogflowCredentials = require("../utils/keys");
 const Orders = require("../models/orderModel");
 const Books = require("../models/bookModel");
+const Categories = require("../models/categoryModel");
 
 const projectId = dialogflowCredentials.projectId;
 const languageCode = dialogflowCredentials.sessionLanguageCode;
@@ -14,6 +15,9 @@ const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
 
 //#region Action definement
 const BEST_SELLING_ACTION = "bestSell";
+const NEW_ARRIVALS_ACTION = "newArrivals";
+const CATEGORY_SEARCH_ACTION = "categorySearch";
+const SEARCH_BOOK_BY_CATEGORY = "searchBookByCate";
 //#endregion
 
 const chatbotController = {
@@ -36,14 +40,23 @@ const chatbotController = {
 
     switch (action) {
       case BEST_SELLING_ACTION:
-        bestSellInMonth(req, res);
+        bestSellInMonth(res);
+        break;
+      case NEW_ARRIVALS_ACTION:
+        newArrivals(res);
+        break;
+      case CATEGORY_SEARCH_ACTION:
+        categorySearch(res);
+        break;
+      case SEARCH_BOOK_BY_CATEGORY:
+        searchBookByCate(res, responses);
         break;
       case "makeOrder":
-        makeOrder(req, res, responses);
+        makeOrder(res, responses);
         break;
       default:
-        const result = responses[0].queryResult;
-        res.status(200).json(result);
+        defaultAction(res, responses);
+        break;
     }
 
     // const result = responses[0].queryResult;
@@ -71,7 +84,16 @@ const chatbotController = {
   },
 };
 
-async function bestSellInMonth(req, res) {
+function defaultAction(res, responses) {
+  const text = responses[0].queryResult.fulfillmentMessages[0].text.text[0];
+
+  res.status(200).json({
+    type: 1,
+    text: text,
+  });
+}
+
+async function bestSellInMonth(res) {
   const from = new Date(new Date().getFullYear(), new Date().getMonth());
   const to = new Date(new Date().getFullYear(), new Date().getMonth() + 1);
 
@@ -113,18 +135,73 @@ async function bestSellInMonth(req, res) {
       }
     });
 
-    const { name, author, imageUrl, isAvailable, price } = await Books.findById(
-      bestSellId
-    );
+    const book = await Books.findById(bestSellId);
 
     res.status(200).json({
-      msg: "This book has the most selling this month",
-      data: { name, author, imageUrl, isAvailable, price },
+      type: 2,
+      text: "This book has the most selling this month",
+      data: [book],
     });
   }
 }
 
-async function makeOrder(req, res, responses) {
+async function newArrivals(res) {
+  const books = await Books.find().sort({ createdAt: 1 });
+
+  if (books.length < 1) {
+    res.status(200).json({
+      type: 1,
+      text: "Sorry, we have not enough data to show you.",
+    });
+  } else {
+    res.status(200).json({
+      type: 2,
+      text: "We have 3 new arrivals for you.",
+      data: books.slice(0, 3),
+    });
+  }
+}
+
+async function categorySearch(res) {
+  const categories = await Categories.find();
+
+  if (!categories) {
+    res.status(200).json({
+      type: 1,
+      text: "Sorry, we have not enough data to show you.",
+    });
+  } else {
+    res.status(200).json({
+      type: 3,
+      text: "We have some kinds of book for you.",
+      data: categories,
+    });
+  }
+}
+
+async function searchBookByCate(res, responses) {
+  const categoryName =
+    responses[0].queryResult.parameters.fields.categoryName.stringValue;
+
+  const category = await Categories.findOne({ categoryName: categoryName });
+
+  if (!category) {
+    res.status(200).json({
+      type: 1,
+      text: "Sorry, we don't have that kind of book in our store.",
+    });
+  } else {
+    const books = await Books.find({ categoryId: category._id });
+
+    res.status(200).json({
+      type: 2,
+      text: "We recommend some books for you.",
+      data: books,
+    });
+  }
+}
+
+async function makeOrder(res, responses) {
   const result = responses[0].queryResult;
   if (!result.allRequiredParamsPresent) {
     const message = result.fulfillmentMessages[0].text.text[0];
