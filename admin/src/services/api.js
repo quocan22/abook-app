6050bfe8903c58fe38f7cf0af61a45cfe380fe36
An Phan <quocan.omg@gmail.com>
2022-06-23 10:19:30 +0700

    adjust environment variables

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -2,7 +2,7 @@ import TokenService from './TokenService';
 
 const axios = require('axios');
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const instance = axios.create({
   baseURL: API_BASE_URL
